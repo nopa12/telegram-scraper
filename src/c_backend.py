d7b307be589f7dc544af3a9f34c28a0587ce1cf6
Aviad Coppenhagen <aviad@steps.me>
2023-10-07 22:45:07 +0300

    Handling connection and missing data

--- src/c_backend.py
+++ src/c_backend.py
@@ -21,35 +21,42 @@ class TgMsgsRawResource(Resource):
         per_page = args['per_page']
         offset = (page - 1) * per_page
 
-        tg_msgs_raw = (
-            db.session.query(db.TgMsgsRaw)
-            .order_by(db.TgMsgsRaw.tg_date.desc())
-            .offset(offset)
-            .limit(per_page)
-            .all()
-        )
-
-        results = []
-        for tg_msg_raw in tg_msgs_raw:
-            tg_media = (
-                db.session.query(db.TgMedia)
-                .filter_by(tg_msg_id=tg_msg_raw.id)
-                .first()
+        with db.transaction_context() as conn:
+            existing_msg = conn.query(db.TgMsgsRaw).first()
+            tg_msgs_raw = (
+                conn.query(db.TgMsgsRaw)
+                .order_by(db.TgMsgsRaw.tg_date.desc())
+                .offset(offset)
+                .limit(per_page)
+                .all()
             )
 
-            result = {
-                'id': str(tg_msg_raw.id),
-                'tg_id': tg_msg_raw.tg_id,
-                'tg_date': tg_msg_raw.tg_date.isoformat(),
-                'tg_chat': tg_msg_raw.tg_chat,
-                'tg_chat_id': tg_msg_raw.tg_chat_id,
-                'tg_msg': tg_msg_raw.tg_msg,
-                'tg_media': {
-                    'id': str(tg_media.id),
-                    'tg_file_path': tg_media.tg_file_path if tg_media else None,
-                },
-            }
-            results.append(result)
+            results = []
+            for tg_msg_raw in tg_msgs_raw:
+                tg_media = (
+                    conn.query(db.TgMedia)
+                    .filter_by(tg_msg_id=tg_msg_raw.id)
+                    .first()
+                )
+
+                media_response = None
+
+                if tg_media:
+                    media_response = {
+                        'id': str(tg_media.id),
+                        'tg_file_path': tg_media.tg_file_path if tg_media else None,
+                    }
+
+                result = {
+                    'id': str(tg_msg_raw.id),
+                    'tg_id': tg_msg_raw.tg_id,
+                    'tg_date': tg_msg_raw.tg_date.isoformat(),
+                    'tg_chat': tg_msg_raw.tg_chat,
+                    'tg_chat_id': tg_msg_raw.tg_chat_id,
+                    'tg_msg': tg_msg_raw.tg_msg,
+                    'tg_media': media_response,
+                }
+                results.append(result)
 
         return {'data': results}
 
